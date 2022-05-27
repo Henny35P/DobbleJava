0bbf025be855722a4c11ed8873b5391b0e18ccca
Hans <hans.villarroel@usach.cl>
2022-05-27 17:16:29 -0400

    Generacion de Mazo como metodo

---
+++ src/main/java/Dobble.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Dobble{
+    int N;
+    public static void main(String[] args) {
+         System.out.println(generacionMazo(7));
+    }
+    static ArrayList<ArrayList<Integer>> generacionMazo(int N) {
+        ArrayList<ArrayList<Integer>> mazo = new ArrayList<ArrayList<Integer>>();
+            ArrayList<Integer> firstCard = new ArrayList<Integer>();
+            int i, j, k;
+            for (i = 1; i <= N + 1; i++) {
+                firstCard.add(i);
+            }
+            mazo.add(firstCard);
+            for (i = 1; i <= N; i++) {
+                ArrayList<Integer> nCards = new ArrayList<Integer>();
+                nCards.add(1);
+                for (j = 1; j <= N; j++) {
+                    nCards.add(i * N + (j + 1));
+                }
+                mazo.add(nCards);
+            }
+            for (i = 1; i <= N; i++) {
+                for (j = 1; j <= N; j++) {
+                    ArrayList<Integer> n2Cards = new ArrayList<Integer>();
+                    n2Cards.add(i + 1);
+                    for (k = 1; k <= N; k++) {
+                        n2Cards.add(N + 2 + N * (k - 1) + (((i - 1) * (k - 1) + j - 1) % N));
+                    }
+                    mazo.add(n2Cards);
+                }
+            }
+            return mazo;
+        }
+    }
