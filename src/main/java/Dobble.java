d6da82fe2b38528dac187cb8caa4a595892d853b
Hans <hans.villarroel@usach.cl>
2022-05-31 22:35:59 -0400

    Constructor DOBBLE mas a fondo

--- src/main/java/Dobble.java
+++ src/main/java/Dobble.java
@@ -2,49 +2,51 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Dobble{
-    int N;
-    int numeroCartas;
+    int numE;
+    int numCartas;
     ArrayList<Object> mazo;
-    ArrayList<String> elementos;
-    int seed;
-
-    public Dobble(ArrayList<String> elementos,int N, int numeroCartas, int seed) {
-    }
+    ArrayList<String> listaElementos;
 
     public static void main(String[] args) {
-        System.out.println(generacionMazo(3));
-        System.out.println(primeraCarta(generacionMazo(3)));
-        System.out.println(numCards(generacionMazo(3)));
-        System.out.println(cardsSetToString(generacionMazo(3)));
+        Dobble dobble = new Dobble(7,30);
     }
+
+    public Dobble(int numElementos, int numeroCartas) {
+        numE = numElementos;
+        numCartas = numeroCartas;
+
+    }
+
     static ArrayList<Object> generacionMazo(int N) {
         ArrayList<Object> mazo = new ArrayList<Object>();
-            ArrayList<Integer> firstCard = new ArrayList<Integer>();
-            int i, j, k;
-            for (i = 1; i <= N + 1; i++) {
-                firstCard.add(i);
+        ArrayList<Integer> firstCard = new ArrayList<Integer>();
+        int i, j, k;
+        for (i = 1; i <= N + 1; i++) {
+            firstCard.add(i);
+        }
+        mazo.add(firstCard);
+        for (i = 1; i <= N; i++) {
+            ArrayList<Integer> nCards = new ArrayList<Integer>();
+            nCards.add(1);
+            for (j = 1; j <= N; j++) {
+                nCards.add(i * N + (j + 1));
             }
-            mazo.add(firstCard);
-            for (i = 1; i <= N; i++) {
-                ArrayList<Integer> nCards = new ArrayList<Integer>();
-                nCards.add(1);
-                for (j = 1; j <= N; j++) {
-                    nCards.add(i * N + (j + 1));
+            mazo.add(nCards);
+        }
+        for (i = 1; i <= N; i++) {
+            for (j = 1; j <= N; j++) {
+                ArrayList<Integer> n2Cards = new ArrayList<Integer>();
+                n2Cards.add(i + 1);
+                for (k = 1; k <= N; k++) {
+                    n2Cards.add(N + 2 + N * (k - 1) + (((i - 1) * (k - 1) + j - 1) % N));
                 }
-                mazo.add(nCards);
+                mazo.add(n2Cards);
             }
-            for (i = 1; i <= N; i++) {
-                for (j = 1; j <= N; j++) {
-                    ArrayList<Integer> n2Cards = new ArrayList<Integer>();
-                    n2Cards.add(i + 1);
-                    for (k = 1; k <= N; k++) {
-                        n2Cards.add(N + 2 + N * (k - 1) + (((i - 1) * (k - 1) + j - 1) % N));
-                    }
-                    mazo.add(n2Cards);
-                }
-            }
-            return mazo;
-        }static int numCards(ArrayList mazo){
+        }
+        return mazo;
+    }
+
+    static int numCards(ArrayList mazo){
             return mazo.size();
         }
     static ArrayList<Object> primeraCarta(ArrayList mazo){
@@ -65,20 +67,12 @@ public class Dobble{
             return CardsSetToString;
         }
 
-    public int getN() {
-        return N;
+    public int getnumCartas() {
+        return numCartas;
     }
 
-    public void setN(int n) {
-        N = n;
-    }
-
-    public int getNumeroCartas() {
-        return numeroCartas;
-    }
-
-    public void setNumeroCartas(int numeroCartas) {
-        this.numeroCartas = numeroCartas;
+    public void setnumCartas(int numCartas) {
+        this.numCartas = numCartas;
     }
 
     public ArrayList<Object> getMazo() {
@@ -89,20 +83,20 @@ public class Dobble{
         this.mazo = mazo;
     }
 
-    public ArrayList<String> getElementos() {
-        return elementos;
+    public int getNumE() {
+        return numE;
     }
 
-    public void setElementos(ArrayList<String> elementos) {
-        this.elementos = elementos;
+    public void setNumE(int numE) {
+        this.numE= numE;
     }
 
-    public int getSeed() {
-        return seed;
+    public ArrayList<String> getListaElementos() {
+        return listaElementos;
     }
 
-    public void setSeed(int seed) {
-        this.seed = seed;
+    public void setListaElementos(ArrayList<String> listaElementos) {
+        this.listaElementos = listaElementos;
     }
 }
 
